4386779e3ba6a3bd12feac4dab08d8526fef7e08
CrackerJaps <thecrackerjaps@gmail.com>
2022-05-13 21:55:37 -0700

    add toolbox to about page - add stack icons to assets folder

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
+import { gsap } from 'gsap'
 import MyToolbox from '../components/MyToolbox'
 
 export default function About() {
+  const about = useRef()
+  const q = gsap.utils.selector(about)
+  var tl = gsap.timeline()
+
+  useEffect(() => {
+    tl.from(q('.stack-icon'), {
+      opacity: 0,
+      duration: 2,
+      stagger: 0.15
+    })
+  }, [])
+
   return (
-    <section className="about section">
+    <section className="about section" ref={about}>
       <h1 className="about__title section-title">
         Hello there, I'm <span className="about__title-span">Sean</span>
       </h1>
