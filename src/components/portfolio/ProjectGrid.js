c61ce02b398501fe88d206c5059cbdc1ef442d7f
CrackerJaps <thecrackerjaps@gmail.com>
2022-05-26 02:05:24 -0700

    animate and prepare grid for responsive

--- src/components/portfolio/ProjectGrid.js
+++ src/components/portfolio/ProjectGrid.js
@@ -1,24 +1,47 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
+import { gsap } from 'gsap'
 import { nanoid } from 'nanoid'
 import projects from '../../data/projects'
 import stackIcons from '../../data/stackIcons'
 
 export default function ProjectGrid() {
+  const gridRef = useRef()
+  const q = gsap.utils.selector(gridRef)
+  const tl = gsap.timeline()
+
+  useEffect(() => {
+    tl.to(q('.caw'), {
+      y: 0,
+      stagger: 0.15,
+      delay: -0.5
+    })
+    tl.to(q('.caw'), {
+      opacity: 1,
+      rotateY: '0deg',
+      stagger: 0.15,
+      delay: -1.5
+    })
+  }, [])
+
   const gridCards = projects.map((proj) => (
-    <div className="project-grid__img-container" key={nanoid()}>
+    <div
+      className="project-grid__img-container caw"
+      key={proj.name}
+      style={{ opacity: '0', transform: 'translateY(-20px) rotateY(180deg)' }}
+    >
       <img
         className="project-grid__img"
         src={proj.img}
         alt={`${proj.name} project preview`}
       />
-      <div className="project-grid__details-div">
+      <div className="project-grid__details">
         <p className="project-grid__details-name section-subtitle">
           {proj.name}
         </p>
         <p className="project-grid__details-stack-text section-text">
           Made with
         </p>
-        <div className="project-grid__details-stack-div">
+        <div className="project-grid__details-stack">
           {proj.stack.map((icon) => {
             const thisIcon = stackIcons.filter((proj) => proj.name === icon)[0]
             return (
@@ -27,32 +50,31 @@ export default function ProjectGrid() {
                 src={thisIcon.img}
                 className={
                   icon === 'React Router' || icon === 'Ableton Live'
-                    ? 'project-grid__stack-icon project-grid__special-icon'
-                    : 'project-grid__stack-icon'
+                    ? 'project-grid__details-stack-icon project-grid__details-stack-icon-special'
+                    : 'project-grid__details-stack-icon'
                 }
                 alt={`${icon} icon`}
               />
             )
           })}
         </div>
-        <div className="project-grid__links-div">
+        <div className="project-grid__links">
           <a
             href={proj.link}
-            className="project-grid__link"
+            className="project-grid__links-link"
             target="_blank"
             referrer="no-referrer"
-            key={nanoid()}
           >
-            <i className="fa-solid fa-arrow-up-right-from-square project-card__link-icon"></i>
+            <i className="fa-solid fa-arrow-up-right-from-square project-card__links-icon"></i>
             Website
           </a>
           <a
-            className="project-grid__link"
+            className="project-grid__links-link"
             href={proj.github}
             target="_blank"
             referrer="noreferrer"
           >
-            <i className="fa-brands fa-github project-grid__link-icon"></i>
+            <i className="fa-brands fa-github project-grid__links-icon"></i>
             Github
           </a>
         </div>
@@ -60,5 +82,9 @@ export default function ProjectGrid() {
     </div>
   ))
 
-  return <div className="project-grid">{gridCards}</div>
+  return (
+    <div className="project-grid" ref={gridRef}>
+      {gridCards}
+    </div>
+  )
 }
